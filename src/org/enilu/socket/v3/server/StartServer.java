e4f7611bf7c4441e9f6857e0e0ae7d8ad711f494
enilu <zhangtao54@gmail.com>
2014-08-05 15:05:23 +0800

    增加服务器端用户启动服务器端的时候输入端口的功能

--- src/org/enilu/socket/v3/server/StartServer.java
+++ src/org/enilu/socket/v3/server/StartServer.java
@@ -1,15 +1,33 @@
 package org.enilu.socket.v3.server;
 
+/**
+ * 
+ * 启动服务器端，使用指定端口启动服务器端
+ * <p>
+ * 
+ * @author enilu(82552623@qq.com)
+ * 
+ */
 public class StartServer {
 
 	/**
+	 * 启动服务器端
+	 * 
 	 * @param args
+	 *            默认第一个参数为监听端口，如果不输入，则使用9999作为默认端口
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
-		Deamon deamon = new Deamon(9999);
+	public static void main(String[] args) throws Exception {
+		int port = 9999;
+		if (args.length > 0) {
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+				throw new Exception("端口必需为数字");
+			}
+		}
+		Deamon deamon = new Deamon(port);
 		new Thread(deamon).start();
-
-		// deamon.setFlag(false);
 	}
 
 }
